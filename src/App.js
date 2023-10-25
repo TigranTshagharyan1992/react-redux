d321b8238db1b3b8124b166bc8287af178c021a3
Tigran <tshagharyant20@gmail.com>
2023-10-25 15:15:45 +0400

    start learning redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+
+  const addCash = (cash) => {
+      dispatch({type:"ADD_CASH",payload: cash})
+  }
+  const getCash = (cash) => {
+      dispatch({type:"GET_CASH",payload: cash})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div style={{fontSize:"3rem"}}>{cash}</div>
+        <div style={{display: "flex"}}>
+          <button onClick={()=>addCash(Number(prompt()))}>add to balance</button>
+          <button onClick={()=>getCash(Number(prompt()))}>take from balance</button>
+        </div>
     </div>
   );
 }
